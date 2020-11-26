1ea5ebcae92fa0d955135b47976c74e2a940bfc6
Jarede Lima <jarede.lima@a.ucb.br>
2020-11-25 21:19:21 -0300

    Creates base project

---
+++ abi-beer-app/abi-beer-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  abi-beer-app
+//
+//  Created by Jarede Lima on 25/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
